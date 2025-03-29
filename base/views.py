c49e746ae99b300d3be247edba462892e4c31d27
Min Choi <minc.low@gmail.com>
2025-03-29 18:28:31 +1100

    update authentication variable

--- base/views.py
+++ base/views.py
@@ -37,7 +37,7 @@ def loginPage(request):
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
