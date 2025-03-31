dddf74b66609511e743b95cb8d875c1dce17ec38
Min Choi <minc.low@gmail.com>
2025-03-31 22:39:29 +1100

    clean up static directory

--- studybud/settings.py
+++ studybud/settings.py
@@ -135,17 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # added directly in live
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
